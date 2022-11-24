5f325b4ebdc346b5d2c88f5db9446f88c3901447
Romina <rominapozzuto@gmail.com>
2022-11-23 23:05:51 -0300

    Init project

---
+++ src/main/java/com/desi/SmnApp/SmnAppApplication.java
@@ -0,0 +1,13 @@
+package com.desi.SmnApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmnAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmnAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desi/SmnApp/SmnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desi.SmnApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmnAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
