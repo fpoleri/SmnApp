91e4a0f8dbbafc0d5bbbe546e74a01c46690779c
Romina <rominapozzuto@gmail.com>
2022-12-07 18:23:09 -0300

    add extended forecast edit

--- src/main/java/com/desi/SmnApp/controllers/extendedForecast/ExtendedForecastForm.java
+++ src/main/java/com/desi/SmnApp/controllers/extendedForecast/ExtendedForecastForm.java
@@ -2,84 +2,94 @@ package com.desi.SmnApp.controllers.extendedForecast;
 
 import java.sql.Date;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import com.desi.SmnApp.entities.ExtendedForecast;
 
-import net.bytebuddy.utility.nullability.NeverNull;
-
 public class ExtendedForecastForm {
-	@NeverNull
-	private Long id;
-	@NeverNull
-	private Long idCity;
-	@NeverNull
-	private Date selectedDate;
-	@NeverNull
-	private int rainProbability;
-	@NeverNull
-	private int predictedRain;
-	@NeverNull
-	private String description;
-	
+    private Long id;
+    @NotNull(message = "Debes seleccionar una ciudad")
+    private Long idCity;
+    @NotNull(message = "La fecha no puede ser nula")
+    private Date date;
+    @NotNull(message = "Debes completar el campo probabilidad de lluvias")
+    private int rainProbability;
+    @NotNull(message = "Debes completa el campo lluvias previstas")
+    private int predictedRain;
+    @NotEmpty(message = "La descripción es obligatoria")
+    private String description;
 
-	public ExtendedForecastForm() {
-		super();
-	}
-	
-	public ExtendedForecastForm(ExtendedForecast e) {
-		super();
-		this.id=e.getId();
-		this.selectedDate=e.getDate();
-		this.rainProbability = e.getRainProbability();
-		this.predictedRain = e.getPredictedRain();
-		this.description = e.getDescription();
-		this.idCity=e.getCity().getId();
-	}
 
-	public ExtendedForecast toPojo()
-	{
-		ExtendedForecast e = new ExtendedForecast();
-		e.setId(this.id);
-		e.setDate(this.getSelectedDate());
-		e.setPredictedRain(this.getPredictedRain());
-		e.setRainProbability(this.getRainProbability());
-		e.setDescription(this.getDescription());
-		return e;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Date getSelectedDate() {
-		return selectedDate;
-	}
-	public void setSelectedDate(Date selectedDate) {
-		this.selectedDate = selectedDate;
-	}
-	public int getRainProbability() {
-		return rainProbability;
-	}
-	public void setRainProbability(int rainProbability) {
-		this.rainProbability = rainProbability;
-	}
-	public int getPredictedRain() {
-		return predictedRain;
-	}
-	public void setPredictedRain(int predictedRain) {
-		this.predictedRain = predictedRain;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Long getIdCity() {
-		return idCity;
-	}
-	public void setIdCity(Long idCity) {
-		this.idCity = idCity;
-	}
+    public ExtendedForecastForm() {
+        super();
+    }
+
+    public ExtendedForecastForm(ExtendedForecast e) {
+        super();
+        this.id = e.getId();
+        this.date = e.getDate();
+        this.rainProbability = e.getRainProbability();
+        this.predictedRain = e.getPredictedRain();
+        this.description = e.getDescription();
+        this.idCity = e.getCity().getId();
+    }
+
+    public ExtendedForecast toPojo() {
+        ExtendedForecast e = new ExtendedForecast();
+        e.setId(this.id);
+        e.setDate(this.getDate());
+        e.setPredictedRain(this.getPredictedRain());
+        e.setRainProbability(this.getRainProbability());
+        e.setDescription(this.getDescription());
+        return e;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getRainProbability() {
+        return rainProbability;
+    }
+
+    public void setRainProbability(int rainProbability) {
+        this.rainProbability = rainProbability;
+    }
+
+    public int getPredictedRain() {
+        return predictedRain;
+    }
+
+    public void setPredictedRain(int predictedRain) {
+        this.predictedRain = predictedRain;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getIdCity() {
+        return idCity;
+    }
+
+    public void setIdCity(Long idCity) {
+        this.idCity = idCity;
+    }
 }
--- src/main/java/com/desi/SmnApp/repositories/IExtendedForecastRepository.java
+++ src/main/java/com/desi/SmnApp/repositories/IExtendedForecastRepository.java
@@ -1,16 +1,20 @@
 package com.desi.SmnApp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.desi.SmnApp.entities.ExtendedForecast;
 
+import java.sql.Date;
 import java.util.List;
 
 @Repository
 public interface IExtendedForecastRepository extends JpaRepository<ExtendedForecast, Long> {
-	
-	List<ExtendedForecast> findByCityId(Long cityId);
-	List<ExtendedForecast> findAll();
-	
+
+    List<ExtendedForecast> findByCityId(Long cityId);
+
+    List<ExtendedForecast> findAll();
+
+    ExtendedForecast findByCityIdAndDate(Long cityId, Date date);
 }
